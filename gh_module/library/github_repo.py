3e10958304b659717500fcf67dfdbeadc87cae21
edpro99 <edpro@pacbell.net>
2023-05-11 19:31:07 +0000

    github changes

--- gh_module/library/github_repo.py
+++ gh_module/library/github_repo.py
@@ -8,6 +8,37 @@ def main():
 	 response = {"hello": "world"}
 	 module.exit_json(changed=False, meta=response)
 
+- name: Create a github Repo
+      github_repo:
+        github_auth_key: "..."
+        username: "YOUR GITHUB USERNAME HERE"
+        name: "Hello-World"
+        description: "This is your first repository"
+        private: yes
+        has_issues: no
+        has_wiki: no
+        has_downloads: no
+        state: present
+      register: result
 
-if __name__ == '__main__':
-    main()
+	
+def main():
+
+    fields = {
+        "github_auth_key": {"required": True, "type": "str"},
+        "username": {"required": True, "type": "str"},
+        "name": {"required": True, "type": "str"},
+        "description": {"required": False, "type": "str"},
+        "private": {"default": False, "type": "bool"},
+        "has_issues": {"default": True, "type": "bool"},
+        "has_wiki": {"default": True, "type": "bool"},
+        "has_downloads": {"default": True, "type": "bool"},
+        "state": {
+            "default": "present",
+            "choices": ['present', 'absent'],
+            "type": 'str'
+        },
+    }
+
+	if __name__ == '__main__':
+		    main()
